19cac2b5a436f755c3c083a292f8a827e477c0e6
rabee333 <rabeea.abusaleh@gmail.com>
2020-01-01 14:08:13 +0200

    task 7

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -14,10 +14,12 @@ namespace GradeBook.GradeBooks
         public string Name { get; set; }
         public List<Student> Students { get; set; }
         public GradeBookType Type { get; set; }
+        public bool IsWeighted { get; set; }
 
-        public BaseGradeBook(string name)
+        public BaseGradeBook(string name, bool isWeighted)
         {
             Name = name;
+            IsWeighted = isWeighted;
             Students = new List<Student>();
         }
 
@@ -107,20 +109,24 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
+            var bonus = 0;
+            if(IsWeighted && studentType == StudentType.Honors)
+                bonus = 1;
+            
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    return 4 + bonus;
                 case 'B':
-                    return 3;
+                    return 3 + bonus;
                 case 'C':
-                    return 2;
+                    return 2 + bonus;
                 case 'D':
-                    return 1;
+                    return 1 + bonus;
                 case 'F':
-                    return 0;
+                    return 0 + bonus;
             }
-            return 0;
+            return 0 + bonus;
         }
 
         public virtual void CalculateStatistics()
