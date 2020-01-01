5242b7b8ffc7cc067f9487f70c359c9946341e65
rabee333 <rabeea.abusaleh@gmail.com>
2020-01-01 13:31:41 +0200

    fix

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -36,7 +36,8 @@ namespace GradeBook.GradeBooks
             if (Students.Count < 5)
                 Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
             
-            base.CalculateStatistics();
+            else
+                base.CalculateStatistics();
         }
 
         public override void CalculateStudentStatistics(string name)
@@ -44,7 +45,8 @@ namespace GradeBook.GradeBooks
             if (Students.Count < 5)
                 Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
 
-            base.CalculateStudentStatistics(name);
+            else
+                base.CalculateStudentStatistics(name);
         }
     }
 }
